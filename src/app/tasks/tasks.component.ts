8cf9dc3d89fccae0356bc3bdbb2811e6f36c9560
Chitrangi Mestry <chitrangidmestrymbp@gmail.com>
2025-07-24 12:30:23 +0530

    Refactor: Changes in the Components.Migrated from Standalone to Module-based. Add: added app.module file.

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output } from '@angular/core';
+import { Component, Input, } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from './new-task/new-task.component';
 import { NewTaskData } from './task/task.model';
@@ -6,8 +6,7 @@ import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
-  standalone: true,
-  imports: [TaskComponent, NewTaskComponent],
+  // standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
